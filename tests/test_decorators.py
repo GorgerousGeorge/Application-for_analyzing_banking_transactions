9987951659fe18a18aecf10d369c0832fd58b5e0
Georgiy Sinitsyn <gorgerousgeorge@gmail.com>
2025-01-12 15:58:34 +0300

    добавлены тесты для декоратора reportwriter

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -5,6 +5,7 @@ import json
 
 testreport = [{'сущность': 'тест', 'номер': 1}, {'сущность': 'проверка', 'номер': '2'}]
 
+
 def test_reportwriter_without_args():
     @reportwriter()
     def some_report():
@@ -12,6 +13,18 @@ def test_reportwriter_without_args():
 
     some_report()
     with open(f"{os.path.dirname(os.getcwd())}\\reports\\Lastreport.json", "r") as file:
+        lines = file.readlines()
+        assert lines[-1] == ('"[{\\"сущность\\": \\"тест\\", \\"номер\\": 1}, {\\"сущность\\": '
+                             '\\"проверка\\", \\"номер\\": \\"2\\"}]"')
+
+
+def test_reportwriter_with_filename():
+    @reportwriter(filename="testreport.json")
+    def some_report():
+        return json.dumps(testreport, ensure_ascii=False)
+
+    some_report()
+    with open(f"{os.path.dirname(os.getcwd())}\\reports\\testreport.json", "r") as file:
         lines = file.readlines()
         assert lines[-1] == ('"[{\\"сущность\\": \\"тест\\", \\"номер\\": 1}, {\\"сущность\\": '
                              '\\"проверка\\", \\"номер\\": \\"2\\"}]"')
