9c8f13456daa297e9cfb365c5efd8f90f4500b34
Georgiy Sinitsyn <gorgerousgeorge@gmail.com>
2025-03-04 20:06:10 +0300

    hotfixes

--- src/views.py
+++ src/views.py
@@ -8,7 +8,8 @@ from src.utils import stock_pricer
 def events(transactions_df, date_str: str, range_type: str = "M"):
     """Функция, которая принимает на вход датафрейм с транзакциями, строку с датой и опционально строковый параметр,
     отвечающий за величину диапазона: W - неделя, на которую приходится дата, M - месяц, Y - год, ALL - все данные до
-    указанной даты (по умолчанию берется месяц)"""
+    указанной даты (по умолчанию берется месяц) и возвращает json с информацией о категориях расходов и пополнений, а
+    также текущими курсами USD и стоимости акций из S&P 500"""
     start_date, end_date = filter_data(date_str, range_type)
     total_expenses, main_expenses, other_expenses, sorted_cash_and_transfer, total_income, sorted_income = (
         calculate_expenses_and_income(transactions_df, start_date, end_date))
